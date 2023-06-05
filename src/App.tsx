c588685c6f5bd9495bfde6ebd247e203bc93ee1b
James Struthers <jamstruth18@gmail.com>
2023-06-05 20:36:34 +0100

    Integratin basic 30s timer card into the Root App

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,32 @@ import Container from '@mui/material/Container';
 import logo from './logo.svg';
 import './App.css';
 
+import { Timer } from './components/Timer';
+import { Card, CardContent } from '@mui/material';
+
 function App() {
   return (
     <Container className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <CardContent>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </CardContent>
+      </Card>
+      <Timer 
+        timerLength={30}
+        timerTrigger={() => console.warn('onExpire called')}
+      />
     </Container>
   );
 }
